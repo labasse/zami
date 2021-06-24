26bd819712ad305e776273944bfcea5110f3c015
labasse <labasse@gmail.com>
2021-06-24 16:38:06 +0200

    Absolute deploy path

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tmpfile=$(mktemp)
-archive=~/$1/$(basename $tmpfile).tar.gz
+archive=$1/$(basename $tmpfile).tar.gz
 tar -czf $tmpfile .
 
-ssh deploy "rm -rf ~/$1/*"
+ssh deploy "rm -rf $1/*"
 scp $tmpfile deploy:$archive
 rm $tmpfile
 ssh deploy "tar xvzf $archive"
